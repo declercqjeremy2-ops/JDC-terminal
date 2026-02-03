504c838687363d7242489c00125b2a07e1d1cde2
Jeremy De clercq <jeremydeclercq@jeremys-mbp.home>
2026-02-03 23:28:14 +0100

    Switch CSS minifier and fix has-selector

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // We gebruiken de standaard minifyer in plaats van lightningcss 
+    // om die vage syntax errors te voorkomen
+    cssMinify: 'esbuild' 
+  }
 })
